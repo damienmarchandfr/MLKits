2e6e30e3adf9bf39ee79425514a3aefa4828b3dc
damienmarchandfr <damien-marchand@outlook.fr>
2020-12-13 16:50:47 +0100

    fist commit

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,32 @@
+/**
+ * Tools : https://stephengrider.github.io/JSPlaygrounds/
+ */
+const PREDICTION_POINT = 300;
+const K = 3;
+
+const data = [];
+
+function distance(point, predictionPoint) {
+  return Math.abs(point - predictionPoint);
+}
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  data.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
+// https://en.wikipedia.org/wiki/K-nearest_neighbors_algorithm
 function runAnalysis() {
-  // Write code here to analyze stuff
-}
+  const bucketNumber = _.chain(data)
+    .map((row) => [distance(row[0], PREDICTION_POINT), row[3]])
+    .sortBy((row) => row[0])
+    .slice(0, K)
+    .countBy((row) => row[1])
+    .toPairs()
+    .sortBy((row) => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value()
 
+   console.log('Your point will probably fall into',bucketNumber) 
+}
